154e5f849a3474234dce75f69884e406a6139604
unknown <kathyrnyoung57@gmail.com>
2023-05-23 23:11:55 -0400

    cleaned up code and fixed bugs to generate logo

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+// const shapes = require("./lib/shape.js");
+const { Square, Circle, Triangle } = require("./lib/shape.js");
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter up to three characters for your logo',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a color for the text of your logo',
+        name: 'textColor',
+    },
+    {
+        type: 'list',
+        message: 'Please select a shape for your logo',
+        name: 'shape',
+        choices: ['Square', 'Circle', 'Triangle']
+    },
+    {
+        type: 'input',
+        message: 'Please enter a fill color for your shape',
+        name: 'shapeColor',
+    },
+]
+function writeFile(filename, logo) {
+    fs.writeFile(`./example-svg/${filename}.svg`, logo, (err) => {
+        if (err) throw err;
+        console.log('File has been created');
+    });
+}
+
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(response => {
+        if (response.shape === 'Square') {
+            let square = new Square(response.text, response.textColor, response.shapeColor);
+            writeFile('logo', square.render());
+          } else if (response.shape === 'Circle') {
+            let circle = new Circle(response.text, response.textColor, response.shapeColor);
+            writeFile('logo', circle.render());
+          } else if (response.shape === 'Triangle') {
+            let triangle = new Triangle(response.text, response.textColor, response.shapeColor);
+            writeFile('logo', triangle.render());
+        }
+    })
+}
+    init();
